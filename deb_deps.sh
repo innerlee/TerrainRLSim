e4e228709ecf16422c111859601c6f1f8a43aa3f
lizz <363664470@qq.com>
2018-12-19 23:12:04 +0800

    no glog

--- deb_deps.sh
+++ deb_deps.sh
@@ -5,7 +5,7 @@
 # sudo add-apt-repository ppa:oibaf/graphics-drivers
 sudo apt-get install libleveldb-dev libsnappy-dev -y
 sudo apt-get install --no-install-recommends libboost-all-dev -y
-sudo apt-get install libgflags-dev libgoogle-glog-dev -y
+# sudo apt-get install libgflags-dev libgoogle-glog-dev -y
 sudo apt-get install libatlas-base-dev -y
 # sudo apt-get install gcc-4.9-multilib g++-4.9-multilib -y
 sudo apt-get install libf2c2-dev -y
