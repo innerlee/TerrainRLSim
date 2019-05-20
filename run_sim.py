60642cdc20712754e2a26a882eb66a368773657c
innerlee <363664470@qq.com>
2019-05-21 02:17:29 +0800

    tiny
    Signed-off-by: innerlee <363664470@qq.com>

--- run_sim.py
+++ run_sim.py
@@ -1,12 +1,13 @@
 import pyterrain as pt
 
-env = pt.getEnv(env_name="PD_Biped2D_Slopes_Mixed_Terrain-v0", render=True)
+env = pt.getEnv(env_name="PD_Biped2D_Flat_Terrain-v0", render=True)
 env.reset()
 
-for i in range(300):
+for i in range(3000):
     action = env.action_space.sample()
     observation, reward, done, info = env.step(action)
     env.render()
+    print(len(observation))
     print("Reward: ", reward, ", Done: ", done)
     if (done):
         env.reset()
